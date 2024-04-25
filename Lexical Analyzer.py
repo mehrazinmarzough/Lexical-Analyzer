4a2cbef4c4d3031f4e91ce1c05de7be6a42f2564
MehrAzin Marzough <mehrazin.marzough@gmail.com>
2024-04-25 23:38:37 +0330

    numbers code added

--- Lexical Analyzer.py
+++ Lexical Analyzer.py
@@ -111,9 +111,60 @@ def get_comments(m: int):
         index += 1
 
 
+def get_numbers(m: int):
+    index = m
+    state = 0
+
+    while 1:
+        if state == 0:
+            if digit_but_0.__contains__(program[index]):
+                state = 6
+            elif program[index] == "0":
+                state = 1
+            else:
+                return None, None
+        elif state == 1:
+            if program[index] == "x":
+                state = 3
+            else:
+                state = 2
+        elif state == 2:
+            token = Token(program[m:index], "T_Decimal")
+            index += 1
+            return token, index
+        elif state == 3:
+            if hex_digit.__contains__(program[index]):
+                state = 4
+            else:
+                return None, None
+        elif state == 4:
+            if hex_digit.__contains__(program[index]):
+                state = 4
+            else:
+                state = 5
+        elif state == 5:
+            index -= 1
+            token = Token(program[m:index], "T_Hexadecimal")
+            index += 1
+            return token, index
+        elif state == 6:
+            if digit.__contains__(program[index]):
+                state = 6
+            else:
+                state = 7
+        elif state == 7:
+            index -= 1
+            token = Token(program[m:index], "T_Decimal")
+            index += 1
+            return token, index
+        else:
+            return None, None
+
+
 KW = {"bool", "break", "char", "continue", "else", "false", "for", "if", "int", "print", "return", "true"}
 digit = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 hex_digit = digit.union({"A", "B", "C", "D", "E", "F"})
+digit_but_0 = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
 WS = {" ", "\t", "\n"}
 letter_ = set()
 for i in range(65, 91):
@@ -122,6 +173,7 @@ for i in range(97, 123):
     letter_.add(chr(i))
 letter_.add(chr(95))
 letter_digit = letter_.union(digit)
+
 
 lines = []
 while True:
