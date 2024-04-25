0dd9d26813175a9c8217b58d8bd78ef41e47a30c
MehrAzin Marzough <mehrazin.marzough@gmail.com>
2024-04-25 22:16:03 +0330

    notations

--- Lexical Analyzer.py
+++ Lexical Analyzer.py
@@ -15,7 +15,7 @@ def get_ids_or_keywords(m: int):
             if letter_.__contains__(program[index]):
                 state = 1
             else:
-                return None
+                return None, None
         elif state == 1:
             if letter_digit.__contains__(program[index]):
                 state = 1
@@ -29,6 +29,57 @@ def get_ids_or_keywords(m: int):
                 return Token(token, f'T_{token.capitalize()}'), index
             else:
                 return Token(token, "T_Id"), index
+        index += 1
+
+
+def get_notations(m: int):
+    index = m
+    state = 0
+    while 1:
+        if state == 0:
+            if program[index] == "{":
+                state = 1
+            elif program[index] == "}":
+                state = 2
+            elif program[index] == "(":
+                state = 3
+            elif program[index] == ")":
+                state = 4
+            elif program[index] == "[":
+                state = 5
+            elif program[index] == "]":
+                state = 6
+            elif program[index] == ",":
+                state = 7
+            elif program[index] == ";":
+                state = 8
+
+        elif state == 1:
+            index += 1
+            return Token("{", "T_LC"), index
+        elif state == 2:
+            index += 1
+            return Token("}", "T_RC"), index
+        elif state == 3:
+            index += 1
+            return Token("(", "T_LP"), index
+        elif state == 4:
+            index += 1
+            return Token(")", "T_RP"), index
+        elif state == 5:
+            index += 1
+            return Token("[", "T_LB"), index
+        elif state == 6:
+            index += 1
+            return Token("]", "T_RB"), index
+        elif state == 7:
+            index += 1
+            return Token(",", "T_Comma"), index
+        elif state == 8:
+            index += 1
+            return Token(";", "T_Semicolon"), index
+        else:
+            return None, None
 
 
 KW = {"bool", "break", "char", "continue", "else", "false", "for", "if", "int", "print", "return", "true"}
