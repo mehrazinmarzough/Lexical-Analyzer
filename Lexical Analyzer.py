c0e03fb6526f315492e7ce5fc8e5db92547b974e
MehrAzin Marzough <mehrazin.marzough@gmail.com>
2024-04-25 21:57:50 +0330

    keyword and id code added

--- Lexical Analyzer.py
+++ Lexical Analyzer.py
@@ -7,6 +7,30 @@ class Token:
         print(f'{self.lexeme} -> {self.token}')
 
 
+def get_ids_or_keywords(m: int):
+    index = m
+    state = 0
+    while 1:
+        if state == 0:
+            if letter_.__contains__(program[index]):
+                state = 1
+            else:
+                return None
+        elif state == 1:
+            if letter_digit.__contains__(program[index]):
+                state = 1
+            else:
+                state = 2
+        elif state == 2:
+            index -= 1
+            token = program[m:index]
+            index += 1
+            if KW.__contains__(token):
+                return Token(token, f'T_{token.capitalize()}'), index
+            else:
+                return Token(token, "T_Id"), index
+
+
 KW = {"bool", "break", "char", "continue", "else", "false", "for", "if", "int", "print", "return", "true"}
 digit = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 hex_digit = digit.union({"A", "B", "C", "D", "E", "F"})
@@ -18,11 +42,6 @@ for i in range(97, 123):
     letter_.add(chr(i))
 letter_.add(chr(95))
 letter_digit = letter_.union(digit)
-notations = {"{", "}", "(", ")", "[", "]", ",", ";"}
-operators = {"+", "-", "*", "/", "%", ">", "<", "=", "!", "&", "|"}
-arithmetic_operators = {"+", "-", "*", "/", "%"}
-delimiter = WS.union(notations).union(operators)
-
 
 lines = []
 while True:
