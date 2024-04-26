760177c78e88fa914cd400e89fde3579c20266ae
MehrAzin Marzough <mehrazin.marzough@gmail.com>
2024-04-26 14:06:08 +0330

    operators code added

--- Lexical Analyzer.py
+++ Lexical Analyzer.py
@@ -202,6 +202,141 @@ def get_literals(m: int):
         index += 1
 
 
+def get_operators(m: int):
+    index = m
+    state = 0
+    while 1:
+        c = program[index]
+        if state == 0:
+            if c == "+":
+                state = 1
+            elif c == "-":
+                state = 2
+            elif c == "*":
+                state = 3
+            elif c == "/":
+                state = 4
+            elif c == "%":
+                state = 5
+            elif c == ">":
+                state = 10
+            elif c == "<":
+                state = 13
+            elif c == "=":
+                state = 16
+            elif c == "!":
+                state = 19
+            elif c == "&":
+                state = 22
+            elif c == "|":
+                state = 24
+            else:
+                return None, None
+        elif state == 1:
+            if digit.__contains__(c):
+                state = 6
+            else:
+                state = 8
+        elif state == 2:
+            if digit.__contains__(c):
+                state = 6
+            else:
+                state = 9
+        elif state == 3:
+            token = Token(program[m:index], "T_LOp_ML")
+            return token, index
+        elif state == 4:
+            token = Token(program[m:index], "T_LOp_DV")
+            return token, index
+        elif state == 5:
+            token = Token(program[m:index], "T_LOp_RM")
+            return token, index
+        elif state == 6:
+            if digit.__contains__(c):
+                state = 6
+            else:
+                state = 7
+        elif state == 7:
+            index -= 1
+            token = Token(program[m:index], "T_Decimal")
+            return token, index
+        elif state == 8:
+            index -= 1
+            token = Token(program[m:index], "T_Lop_PL")
+            return token, index
+        elif state == 9:
+            index -= 1
+            token = Token(program[m:index], "T_LOp_MN")
+            return token, index
+        elif state == 10:
+            if c == "=":
+                state = 11
+            else:
+                state = 12
+        elif state == 11:
+            token = Token(program[m:index], "T_LOp_GE")
+            return token, index
+        elif state == 12:
+            index -= 1
+            token = Token(program[m:index], "T_LOp_G")
+            return token, index
+        elif state == 13:
+            if c == "=":
+                state = 14
+            else:
+                state = 15
+        elif state == 14:
+            token = Token(program[m:index], "T_LOp_LE")
+            return token, index
+        elif state == 15:
+            index -= 1
+            token = Token(program[m:index], "T_LOp_L")
+            return token, index
+        elif state == 16:
+            if c == "=":
+                state = 17
+            else:
+                state = 18
+        elif state == 17:
+            token = Token(program[m:index], "T_LOp_E")
+            return token, index
+        elif state == 18:
+            index -= 1
+            token = Token(program[m:index], "T_Assign")
+            return token, index
+        elif state == 19:
+            if c == "=":
+                state = 20
+            else:
+                state = 21
+        elif state == 20:
+            token = Token(program[m:index], "T_LOp_NE")
+            return token, index
+        elif state == 21:
+            index -= 1
+            token = Token(program[m:index], "T_LOp_NOT")
+            return token, index
+        elif state == 22:
+            if c == "&":
+                state = 23
+            else:
+                return None, None
+        elif state == 23:
+            token = Token(program[m:index], "T_LOp_AND")
+            return token, index
+        elif state == 24:
+            if c == "|":
+                state = 25
+            else:
+                return None, None
+        elif state == 25:
+            token = Token(program[m:index], "T_LOp_OR")
+            return token, index
+        else:
+            return None, None
+        index += 1
+
+
 KW = {"bool", "break", "char", "continue", "else", "false", "for", "if", "int", "print", "return", "true"}
 digit = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 hex_digit = digit.union({"A", "B", "C", "D", "E", "F"})
